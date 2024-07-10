7ca53c50575b6b716c940dd191922c686a2c500f
tylerbrooker1 <tylerbrooker2016@outlook.com>
2024-07-09 20:35:39 -0400

    Added new unit testing methods. Made all methods in GameState class public.

--- GameTests/GameTests.cs
+++ GameTests/GameTests.cs
@@ -1,5 +1,6 @@
 using TicTacToeConsole;
 
+// Unit Testing class for TicTacToe game
 namespace GameTests
 {
     [TestClass]
@@ -29,6 +30,59 @@ namespace GameTests
             game.SwitchPlayer(); // should switch from X to O
             game.SwitchPlayer(); // should switch from O to X
             Assert.AreEqual(Player.X, game.CurrentPlayer);
+        }
+
+        [TestMethod]
+        public void Test_IsGridEmptyAtStart()
+        {
+            game = new GameState();
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.IsTrue(game.CanMakeMove(r,c), "Space {0},{1} is filled", r, c);
+                }
+
+            }
+        }
+
+        [TestMethod]
+        public void Test_IsGridEmptyAtReset()
+        {
+            game = new GameState();
+            game.ResetGameState();
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.IsTrue(game.CanMakeMove(r, c), "Space {0},{1} is filled", r, c);
+                }
+
+            }
+        }
+
+        [TestMethod]
+        public void Test_IsGridFullAfterAGame()
+        {
+            /*
+             
+                         X O X
+            Outcome:     O X O
+                         O X X
+
+             */
+            game = new GameState();
+            game.MakeMove(0, 0);
+            game.MakeMove(0, 1);
+            game.MakeMove(0, 2);
+            game.MakeMove(1, 0);
+            game.MakeMove(1, 1);
+            game.MakeMove(1, 2);
+            game.MakeMove(2, 1);
+            game.MakeMove(2, 0);
+            game.MakeMove(2, 2);
+            game.PrintGrid(); 
+            Assert.IsTrue(game.IsGridFull(), "The grid is not returning as full.");
         }
     }
 }
